860b2c23a77a8cb16513ed95003eeee37ef96cb7
Larry Osborn <larry@topsy.com>
2013-03-14 15:14:23 -0700

    added an example made non-amd friendly

--- example/example-app.js
+++ example/example-app.js
@@ -1,12 +1,12 @@
-require(['../router'], function(Router) {
+require(['../src/router'], function(Router) {
 
     router = new Router();
     router.add_route([
-        { route: /^\/example.html\/$/, callback: sample_callback },
-        { route: /^\/example.html\/apples$/, callback: sample_callback },
-        { route: /^\/example.html\/oranges$/, callback: sample_callback },
-        { route: /^\/example.html\/captured\/(\w+)\/(\w+)$/, matches: ['param1', 'param2'], callback: sample_callback },
-        { route: /^\/example.html\/bypassed$/, callback: sample_callback }
+        { route: /^\/example\/example.html\/$/, callback: sample_callback },
+        { route: /^\/example\/example\/example.html\/apples$/, callback: sample_callback },
+        { route: /^\/example\/example.html\/oranges$/, callback: sample_callback },
+        { route: /^\/example\/example.html\/captured\/(\w+)\/(\w+)$/, matches: ['param1', 'param2'], callback: sample_callback },
+        { route: /^\/example\/example.html\/bypassed$/, callback: sample_callback }
     ]);
 
     router.observe();
